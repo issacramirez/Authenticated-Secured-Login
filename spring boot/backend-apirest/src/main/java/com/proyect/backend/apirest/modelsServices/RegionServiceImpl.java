e8ab3b377ff083e8909447c8bfa2d89286733706
Jose Issac Ramirez Martinez <55167590+issacramirez@users.noreply.github.com>
2021-07-14 18:30:47 -0500

    .

--- spring boot/backend-apirest/src/main/java/com/proyect/backend/apirest/modelsServices/RegionServiceImpl.java
+++ spring boot/backend-apirest/src/main/java/com/proyect/backend/apirest/modelsServices/RegionServiceImpl.java
@@ -9,9 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-
-
-
 @Service
 public class RegionServiceImpl implements IRegionService {
 
