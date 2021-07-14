0ef7b34508a6918ad00c41b6af91bece04fcf25e
Jose Issac Ramirez Martinez <55167590+issacramirez@users.noreply.github.com>
2021-07-14 12:28:45 -0500

    ultimos cambios en subida de foto

--- angular/clientes-app/src/app/clientes/clientes.json.ts
+++ angular/clientes-app/src/app/clientes/clientes.json.ts
@@ -6,13 +6,15 @@ export const CLIENTES: Cliente[] = [
     nombre: 'Jose',
     apellido: 'Ramirez',
     email: 'issac@gmail.com',
-    createAt: '2021-11-15'
+    createAt: '2021-11-15',
+    foto: ''
   },
   {
     id: 2,
     nombre: 'Issac',
     apellido: 'Martinez',
     email: 'pepe@gmail.com',
-    createAt: '2021-11-16'
+    createAt: '2021-11-16',
+    foto: ''
   }
 ];
