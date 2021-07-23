8fdac6a3e339326362d99bfc46701828c8ce0e93
Jose Issac Ramirez Martinez <55167590+issacramirez@users.noreply.github.com>
2021-07-22 19:52:22 -0500

    configuracion spring security y autorizacion

---
+++ spring boot/backend-apirest/src/main/java/com/proyect/backend/apirest/auth/AuthorizationServerConfig.java
@@ -0,0 +1,66 @@
+package com.proyect.backend.apirest.auth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
+    @Qualifier("authenticationManager")
+    private AuthenticationManager authenticationManager;
+
+    // se encarga de todo el proceso de autenticación y de validar el token
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager).tokenStore(tokenStore()).accessTokenConverter(accessTokenConverter());
+    }
+
+    // esta funcion es opcional ya que .tokeStore(tokenStore()) ya lo ahce por defecto "AuthorizationServerEndpointsConfigurer"
+    @Bean
+    public JwtTokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+        return jwtAccessTokenConverter;
+    }
+
+    // permisos de nuestros endpoints, rutas de acceso
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security.tokenKeyAccess("permitAll()") // genera el token cuando se autentica
+                .checkTokenAccess("isAuthenticated()"); // validar el token
+    }
+
+    // 
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory().withClient("angularapp") // se indica el id de la aplicacion o cliente
+               .secret(passwordEncoder.encode("secret")) // secret o contraseña
+               .scopes("read", "write") // alcance, permisos que se le dan al cliente o aplicacion
+               .authorizedGrantTypes("password", "refresh_token") // tipo de concesion del token, tipo de autenticacion, como se obtiene el token, 
+               // usamos password cuando es con credenciales, cuando los usuarios existen en el backend
+               // concecion refresh token es para un token nuevo
+               .accessTokenValiditySeconds(3600) // tiempo en que caduca el token (en segundos)
+               .refreshTokenValiditySeconds(3600); // tiempo de expiracion del refresh token
+    }
+
+}
---
+++ spring boot/backend-apirest/src/main/java/com/proyect/backend/apirest/auth/ResourceServerConfig.java
@@ -0,0 +1,22 @@
+package com.proyect.backend.apirest.auth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    // reglas del lado de OAuth
+    // nos permite implementar todas las reglas de seguridad de nuestros endpoints hacia los recursos de la aplicacion
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/clientes", "/api/regiones").permitAll() // permite las rutas del metodo
+            .anyRequest().authenticated() // cualquier otra ruta se requiere autenticación
+        ;
+    }
+    
+}
